662872c838b1c93d16391b0aa4d37c70433017a1
AdelElayyan <adel.elayyan4@gmail.com>
2020-02-22 17:09:52 +0200

    Tic Tac Toe first commit

---
+++ Tic Tac Toe/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+
+        bool turn = true; //true = x turn ; false = y turn
+        int turn_count = 0;
+        static string player1, player2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static void setPlayerName (string n1, string n2)
+        {
+            player1 = n1;
+            player2 = n2;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.ShowDialog();
+            label1.Text = player1;
+            label3.Text = player2;
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("By Adel Elayyan", "Tic Tac Toe about");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if(turn)
+            {
+                b.Text = "X";
+            }
+            else
+            {
+                b.Text = "O";
+            }
+
+            turn = !turn;
+            b.Enabled = false;
+            turn_count++;
+
+            checkforwinner();
+        }
+
+        private void checkforwinner()
+        {
+            bool there_is_a_winner = false;
+
+
+            // horizental checks
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text)&&(!A1.Enabled))
+                there_is_a_winner = true;
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled))
+                there_is_a_winner = true;
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!C1.Enabled))
+                there_is_a_winner = true;
+
+            // vertical checks
+            else if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
+                there_is_a_winner = true;
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
+                there_is_a_winner = true;
+            else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled))
+                there_is_a_winner = true;
+
+            // diagonal checks
+            if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
+                there_is_a_winner = true;
+            else if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (!C1.Enabled))
+                there_is_a_winner = true;
+
+
+            if (there_is_a_winner)
+            {
+                disableButtons();
+
+                string winner = "";
+                if (turn)
+                {
+                    winner = player2;
+                    o_win_count.Text = (Int32.Parse(o_win_count.Text) + 1).ToString();
+                }
+                else
+                {
+                    winner = player1;
+                    x_win_count.Text = (Int32.Parse(x_win_count.Text) + 1).ToString();
+                }
+                MessageBox.Show(winner + " Wins!", "Yay!");
+            }//end if
+            else
+            {
+                if (turn_count == 9)
+                {
+                    MessageBox.Show("!Draw", "Bummer!");
+                    draw_count.Text = (Int32.Parse(draw_count.Text) + 1).ToString();
+                }
+            }
+
+        }//end checkforwinner
+
+        private void disableButtons()
+        {
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                }//end foreach
+            }//end try
+            catch { }
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            turn = true;
+            turn_count = 0;
+
+
+            foreach (Control c in Controls)
+            {
+                try
+                {
+                    Button b = (Button)c;
+                    b.Enabled = true;
+                    b.Text = "";
+                }//end try
+                catch { }
+            }//end foreach
+
+
+        }
+
+        private void button_enter(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if(b.Enabled)
+            {
+                if (turn)
+                    b.Text = "X";
+                else
+                    b.Text = "O";
+            }//end if
+
+        }
+
+        private void button_leave(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (b.Enabled)
+                b.Text = "";
+        }
+
+        private void restToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            o_win_count.Text = "0";
+            x_win_count.Text = "0";
+            draw_count.Text = "0";
+        }
+    }
+}
